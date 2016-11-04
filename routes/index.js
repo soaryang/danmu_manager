5209e76e92aea2804802bccd889b5d394bb85195
soaryang <759620299@qq.com>
2016-11-04 10:45:33 +0800

    聚时代后台管理系统（node）提交

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/main', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  var html = new ejs({url: "/common/header.html"}).render(data);
-  $("#contentContainer").html(html);
-});
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
